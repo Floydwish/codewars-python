686fb454edd119c834e2b38fc5602a3b826f51be
xubo@surfilter.com <my0127912@hotmail.com>
2021-12-07 09:52:59 +0800

    Create 8.Remove String Spaces.py

---
+++ 8kyu/8.Remove String Spaces.py
@@ -0,0 +1,14 @@
+'''
+8.Remove String Spaces
+
+Instructions
+
+Simple, remove the spaces from the string, then return the resultant string.
+'''
+
+#solution 1
+#使用string的replace接口替换所有空字符
+def no_space(x):
+    return x.replace(' ','')
+    
+
