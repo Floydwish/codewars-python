633cc0bf2cc1b1bfede6a5baf32b2c228b1afe36
xubo@surfilter.com <my0127912@hotmail.com>
2021-12-07 09:44:56 +0800

    Create 7.Convert a Number to a String.py

---
+++ 8kyu/7.Convert a Number to a String.py
@@ -0,0 +1,21 @@
+'''
+7.Convert a Number to a String!
+
+Instructions
+
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+123 --> "123"
+999 --> "999"
+
+'''
+
+#solution 1
+#使用内置str直接转换
+def number_to_string(num):
+    return str(num)
+
+
