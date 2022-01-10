c4a2888b36c676b3d0f2c727a6457aabb17ed9b9
xubo@surfilter.com <my0127912@hotmail.com>
2022-01-10 14:40:36 +0800

    Create 21.Remove the minimum.py

---
+++ 7kyu/21.Remove the minimum.py
@@ -0,0 +1,45 @@
+'''
+21.Remove the minimum
+
+The museum of incredible dull things
+The museum of incredible dull things wants to get rid of some exhibitions. 
+Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. 
+She gives them a rating, and then removes the one with the lowest rating.
+
+However, just as she finished rating all exhibitions, she's off to an important fair, 
+so she asks you to write a program that tells her the ratings of the items after one 
+removed the lowest one. Fair enough.
+
+Task
+Given an array of integers, remove the smallest value. Do not mutate the original array/list. 
+If there are multiple elements with the same value, remove the one with a lower index. 
+If you get an empty array/list, return an empty array/list.
+
+Don't change the order of the elements that are left.
+
+Examples
+remove_smallest([1,2,3,4,5]) = [2,3,4,5]
+remove_smallest([5,3,2,1,4]) = [5,3,2,4]
+remove_smallest([2,2,1,2,1]) = [2,2,2,1]
+
+'''
+
+#solution 1
+#1.输入list为空时返回空
+#2.新建list,复制输入list
+#3.使用内置min计算list最小值,使用list函数remove移除最小值
+def remove_smallest(numbers):
+    if not numbers: return []
+    ret = list(numbers)
+    ret.remove(min(ret))
+    return ret 
+
+
+#solution 1
+#1.新建list,复制输入list
+#2.使用内置min计算list最小值,使用list函数remove移除最小值
+def remove_smallest(numbers):
+    ret = list(numbers)
+    if numbers:
+        ret.remove(min(ret))
+    return ret
